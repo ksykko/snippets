93a4c9a6550fe17ea4f5335099988133404bfb04
Ken Sagala <sagala.krch@gmail.com>
2024-02-01 00:14:02 +0800

    02 005 Adding a Creation Form

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,3 +1,30 @@
 export default function SnippetCreatePage() {
-    return <div>Create a Snippet</div>
+    return (
+        <form>
+            <h3 className='font-bold m-3'>Create a Snippet</h3>
+            <div className='flex flex-col gap-4'>
+                <div className='flex gap-4'>
+                    <label htmlFor='title' className='w-12'>
+                        Title
+                    </label>
+                    <input
+                        type='text'
+                        id='title'
+                        name='title'
+                        className='border rounded p-2 w-full'
+                    />
+                </div>
+                <div className='flex gap-4'>
+                    <label htmlFor='code' className='w-12'>
+                        Code
+                    </label>
+                    <textarea id='code' name='code' className='border rounded p-2 w-full' />
+                </div>
+
+                <button type='submit' className='rounded p-2 bg-blue-200'>
+                    Create
+                </button>
+            </div>
+        </form>
+    )
 }
