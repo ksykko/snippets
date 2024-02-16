7934aa88ba9a2f1d3f88e580ff98acda6cc898b8
Ken Sagala <sagala.krch@gmail.com>
2024-02-16 22:51:53 +0800

    03 009 A Few Quick Tasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
+import Link from 'next/link'
 import { db } from '@/db'
 
 export default async function Home() {
     const snippets = await db.snippet.findMany()
 
     const renderedSnippets = snippets.map((snippet) => {
-        return <div key={snippet.id}>{snippet.title}</div>
+        return (
+            <Link
+                key={snippet.id}
+                href={`/snippets/${snippet.id}`}
+                className='flex justify-between items-center p-2 border rounded'>
+                {snippet.title}
+                <div>View</div>
+            </Link>
+        )
     })
 
-    return <div>{renderedSnippets}</div>
+    return (
+        <div>
+            <div className='flex m-2 justify-between items-center'>
+                <h1 className='text-xl font-bold'>Snippets</h1>
+                <Link href='/snippets/new' className='p-2 border rounded'>
+                    New
+                </Link>
+            </div>
+            <div className='flex flex-col gap-2'>{renderedSnippets}</div>
+        </div>
+    )
 }
