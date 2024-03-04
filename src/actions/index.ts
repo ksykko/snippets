5a451d3cbd6c60004de6c1827e7b0d66c1ead0e4
Ken Sagala <sagala.krch@gmail.com>
2024-03-04 23:58:59 +0800

    05 003 Adding the Form Validation

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -19,22 +19,26 @@ export async function deleteSnippet(id: number) {
 }
 
 export async function createSnippet(formState: { message: string }, formData: FormData) {
-    return {
-        message: 'Title must be longer',
+    // Check the user's inputs and make sure they're valid
+    const title = formData.get('title') as string
+    const code = formData.get('code') as string
+
+    if (typeof title !== 'string' || title.length < 3) {
+        return { message: 'Title must be at least 3 characters long' }
     }
 
-    // // Check the user's inputs and make sure they're valid
-    // const title = formData.get('title') as string
-    // const code = formData.get('code') as string
+    if (typeof code !== 'string' || code.length < 10) {
+        return { message: 'Code must be longer' }
+    }
 
-    // // Create a new record in the database
-    // const snippet = await db.snippet.create({
-    //     data: {
-    //         title,
-    //         code,
-    //     },
-    // })
+    // Create a new record in the database
+    const snippet = await db.snippet.create({
+        data: {
+            title,
+            code,
+        },
+    })
 
-    // // Redirect the user back to the root route
-    // redirect('/')
+    // Redirect the user back to the root route
+    redirect('/')
 }
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -28,7 +28,11 @@ export default function SnippetCreatePage() {
                     <textarea id='code' name='code' className='border rounded p-2 w-full' />
                 </div>
 
-                <div>{formState.message}</div>
+                {formState.message ? (
+                    <div className='my-2 p-2 bg-red-200 border rounded border-red-400'>
+                        {formState.message}
+                    </div>
+                ) : null}
 
                 <button type='submit' className='rounded p-2 bg-blue-200'>
                     Create
